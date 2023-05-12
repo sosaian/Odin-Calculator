0e7934e32d9aee7099592cc1ca8de606f8273720
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-05-12 20:55:59 -0300

    Comment functions and variables I will be using later

---
+++ script.js
@@ -0,0 +1,10 @@
+//function add() {}
+//function substract() {}
+//function multiply() {}
+//function divide() {}
+
+//let number1 = ???
+//let operator = ???
+//let number2 = ???
+
+//function operate(operator, number1, number2) {}
