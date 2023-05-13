57c925c049e993b8c384ba715609978bb7d3ee8e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-05-12 21:25:23 -0300

    Add basic logic to buttons and display

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let number1 = 0;
+let operator = "";
+let number2 = 0;
+
+const display = document.querySelector("#display");
+let displayValue = `${number1.toString()} ${operator} ${number2.toString()}`;
+// display.textContent = displayValue;
+
+//function buttonResponse(buttonID) {}
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+buttons.forEach( button =>
+{
+    button.addEventListener("click", console.log(button.id));
+});
+
 //function add() {}
 //function substract() {}
 //function multiply() {}
 //function divide() {}
-
-//let number1 = ???
-//let operator = ???
-//let number2 = ???
-
 //function operate(operator, number1, number2) {}
